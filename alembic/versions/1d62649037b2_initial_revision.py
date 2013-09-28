c9a2cd18bcab2ac58e4a766d86b58e8c9e377bfe
Aleks Kamko <aykamko@gmail.com>
2013-09-28 15:05:11 -0700

    Added alembic to upgrade db, got everything set up (it seems)

---
+++ alembic/versions/1d62649037b2_initial_revision.py
@@ -0,0 +1,22 @@
+"""initial revision
+
+Revision ID: 1d62649037b2
+Revises: None
+Create Date: 2013-09-28 15:03:28.653113
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1d62649037b2'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
