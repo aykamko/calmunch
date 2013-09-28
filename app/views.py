c511b7f1aefe5ad5d19999322669dfdf54f654be
Noah Shafi <noshaf@gmail.com>
2013-09-28 15:35:42 -0700

    Added index and layout

--- app/views.py
+++ app/views.py
@@ -5,3 +5,7 @@ from app import app
 @app.route('/')
 def index():
     return render_template('index.html');
+
+@app.route('/about_us')
+def about_us():
+    return render_template('about_us.html');
