e5650ac72d59421512c931b20b81bee5d1b0c5b0
Max <max@ubuntu.(none)>
2013-09-28 17:44:57 -0700

    database query and index

--- app/views.py
+++ app/views.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from app import app
+from app.models import Event
+from app.database import db_session
 
 @app.route('/')
 def index():
-    return render_template('index.html');
+  event_query = db_session.query(Event).all()
+  return render_template('index.html', event_query = event_query);
 
 @app.route('/about_us')
 def about_us():
