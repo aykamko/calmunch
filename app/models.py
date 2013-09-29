2e58a685da3e197a09826fd25a2c1ad042302672
Krishna Parashar <kcparashar@gmail.com>
2013-09-28 21:45:15 -0700

    Added food images

--- app/models.py
+++ app/models.py
@@ -7,13 +7,15 @@ class Event(Base):
     __tablename__ = 'event'
     id = Column(Integer, primary_key = True)
     name = Column(Unicode(64))
-    blurb = Column(Unicode(140))
-    location = Column(Unicode(140))
     sponsor = Column(Unicode(140))
-    latitude = Column(Float)
-    longitude = Column(Float)
+    blurb = Column(Unicode(140))
     description = Column(Unicode(5000))
     food = Column(Unicode(280))
+
+    location = Column(Unicode(140))
+    latitude = Column(Float)
+    longitude = Column(Float)
+
     start_time = Column(DateTime)
     end_time = Column(DateTime)
     contact = Column(Unicode(140))
